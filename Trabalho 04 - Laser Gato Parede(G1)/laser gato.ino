a232e20a5e9bd707be098806e899c97733d32768
Kuuh4 <73321124+Kuuh4@users.noreply.github.com>
2022-10-28 12:27:19 -0300

    lol

--- Trabalho 04 - Laser Gato Parede(G1)/laser gato.ino
+++ Trabalho 04 - Laser Gato Parede(G1)/laser gato.ino
@@ -16,13 +16,14 @@ void setup() {
 }
 
 void loop() {
+  //Eixo Principal
   valX = analogRead(potX);            // reads the value of the potentiometer (value between 0 and 1023)
   valX = map(valX, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   eixoX.write(valX);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
-
+  //Eixo Secundário
   valY = analogRead(potY);            // reads the value of the potentiometer (value between 0 and 1023)
   valY = map(valY, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   eixoY.write(valY);                  // sets the servo position according to the scaled value
-  delay(15);  
+  delay(15);                            // waits for the servo to get there
 }
