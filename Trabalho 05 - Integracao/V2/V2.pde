ecd6a700451b59adbaf00e7a87b1e99fdb6975d9
Derick-(Life_M1-2) <nfaria2019@gmail.com>
2022-11-09 15:01:55 -0300

    vamo

--- Trabalho 05 - Integracao/V2/V2.pde
+++ Trabalho 05 - Integracao/V2/V2.pde
@@ -1,7 +1,17 @@
-void setup() {
-    
+// Declare and construct two objects (h1, h2) from the class HLine 
+
+Bola b1 = new Bola(100,100);
+
+void setup() 
+{
+  size(200, 200);
+  //fullScreen();
+  frameRate(30);
+  ellipseMode(CENTER);    
 }
 
-void draw() {
-    Bola
-}
+void draw() { 
+  background(204);
+  b1.update();
+} 
+ 
--- Trabalho 05 - Integracao/V2/out/source/V2.java
+++ Trabalho 05 - Integracao/V2/out/source/V2.java
@@ -15,14 +15,82 @@ import java.io.IOException;
 
 public class V2 extends PApplet {
 
- public void setup() {
-    
+// Declare and construct two objects (h1, h2) from the class HLine 
+
+Bola b1 = new Bola(mouseX,mouseY);
+
+ public void setup() 
+{
+  /* size commented out by preprocessor */;
+  //fullScreen();
+  frameRate(30);
+  ellipseMode(CENTER);    
 }
 
- public void draw() {
-    Bola
+ public void draw() { 
+  background(204);
+  b1.update();
+} 
+ 
+/*
+w - width da bola
+h - height da bola
+x - x position
+y - y position
+arrancada - arrancada 0 à 1
+targetX - destino x
+targetY - destino y
+dx - distância entre x atual e destino x
+dy - distância entre y atual e destino y
+distancia_mouse - distãncia linear do ponto até o mouse, p/ variação de cor
+*/
+
+/*
+class Bola {
+  float x,y,arrancada,w,h,targetX,targetY,dx,dy,distancia_mouse;
+
+  Bola(float w, float h, float arrancada){
+  
+  float targetX = mouseX;
+    float dx = targetX - x;
+  float targetY = mouseY;
+    float dy = targetY - y; 
+
+  float distancia_mouse = dist(mouseX, mouseY, x, y);
+  
+  } 
+
+  void update(){
+  
+  if (distancia_mouse<= 0.5){
+    x = mouseX;
+    y = mouseY;
+  } else {
+    x += dx * arrancada;
+    y += dy * arrancada;
+  }
+
+  ellipse(x, y, w, h);
+
+  }
+
+}
+*/
+
+
+class Bola{
+  float posX, posY;
+  Bola(float x, float y){
+   
+    }
+   public void update(){
+
+    ellipse(posX, posY, 20, 20);
+  }
 }
 
+
+  public void settings() { size(200, 200); }
 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "V2" };
