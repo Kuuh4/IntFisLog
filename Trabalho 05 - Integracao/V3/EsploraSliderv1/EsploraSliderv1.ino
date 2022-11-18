fd223a33cf3f6112e1bc50a48f0f568eb8ae6410
Derick-(Life_M1-2) <nfaria2019@gmail.com>
2022-11-18 18:28:45 -0300

    ok

--- Trabalho 05 - Integracao/V3/EsploraSliderv1/EsploraSliderv1.ino
+++ Trabalho 05 - Integracao/V3/EsploraSliderv1/EsploraSliderv1.ino
@@ -3,39 +3,30 @@
 #include <Esplora.h>
 // https://docs.arduino.cc/retired/archived-libraries/EsploraLibrary
 
+#include <Mouse.h>
+
 int modo = 0;
 
 void setup()
 {
   Serial.begin(9600); 
   AbsMouse.init(1920, 1080);
+  Mouse.begin();
+
+
   int valueY = 0;
 }
 
 void loop()
 {
-  int button = Esplora.readButton(1); 
-
-  for(;modo == 1;){
-      
-    int button = Esplora.readButton(1); 
-
-      if (button == 0)  {
-        modo = 0;   
-        Esplora.writeRGB(255, 0, 255);
-        delay(1000);
-      }
-       delay(100);
-  }
-
   for(;modo == 0;){
 
     int button = Esplora.readButton(1); 
   
       if (button == 0)  {
         modo = 1;
-        Esplora.writeRGB(0, 255, 0);
-        delay(1000);
+        Esplora.writeRGB(255, 0, 0);
+        delay(100);
       }
 
       int valueX = (1023-Esplora.readSlider()) +448;     
@@ -44,6 +35,43 @@ void loop()
       
       delay(20);
   }
+
+  for(;modo == 1;){
+        
+      int button = Esplora.readButton(1); 
+
+        if (button == 0)  {
+          modo = 2;   
+          Esplora.writeRGB(0, 255, 0);
+          delay(100);
+        }
+        delay(100);
+    }
+
+    for(;modo == 2;){
+        
+      int button = Esplora.readButton(1); 
+
+        if (button == 0)  {
+          modo = 0;   
+          Esplora.writeRGB(0, 255, 255);
+          delay(100);
+        }
+
+        int xValue = Esplora.readJoystickX();        // read the joystick's X position
+        int yValue = Esplora.readJoystickY();  
+        int mouseX = map(xValue, -512, 512, 10, -10);  // map the X value to a range of movement for the mouse X
+        int mouseY = map(yValue, -512, 512, -10, 10);
+        Mouse.move(mouseX, mouseY, 0); 
+
+          if (Esplora.readButton(4) == 0) {                           // if the joystick button is pressed
+            Mouse.press();                             // send a mouse click
+          } else {
+            Mouse.release();                           // if it's not pressed, release the mouse button 
+          }
+
+        delay(20);
+    }
 
 
 }
